d1a5a9a609f596ffd278615b97ff4dcc765444b6
anderson <>
2000-12-20 14:19:23 -0400

    Add the TypeMember table

---
+++ TypeMember.sql
@@ -0,0 +1,24 @@
+# MySQL dump 7.1
+#
+# Host: localhost    Database: lsb
+#--------------------------------------------------------
+# Server version	3.22.32
+
+#
+# Table structure for table 'TypeMember'
+#
+DROP TABLE IF EXISTS TypeMember;
+CREATE TABLE TypeMember (
+  TMid int(10) DEFAULT '0' NOT NULL auto_increment,
+  TMname varchar(60) DEFAULT '' NOT NULL,
+  TMtypeid int(10) DEFAULT '0' NOT NULL,
+  TMsize int(10) DEFAULT '0' NOT NULL,
+  TMoffset int(10) DEFAULT '0' NOT NULL,
+  TMposition int(10) DEFAULT '0' NOT NULL,
+  TMcomment varchar(60),
+  TMmemberof int(10) DEFAULT '0' NOT NULL,
+  TMarray varchar(60),
+  PRIMARY KEY (TMid),
+  KEY k_TMposmem (TMposition,TMmemberof)
+);
+
