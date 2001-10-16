44167a209e011fe4a26562b7162cf5652b73a691
anderson <>
2001-10-16 02:32:07 -0400

    Be explicte about access from localhost

--- dbperms.sql
+++ dbperms.sql
@@ -4,32 +4,32 @@
 # lsbuser
 #	This user may browse the DB, but not make any changes.
 #
-# lsbadmin
+# lsbadmin@localhost
 #	This user may make any changes to the DB.
 #
 
 #
 # Open up these tables for the LSB db admin
 #
-GRANT INSERT,UPDATE,DELETE ON lsb.Architecture TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.Constant TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.Header TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.HeaderGroup TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.Interface TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.LibGroup TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.LGInt TO lsbadmin;
+GRANT INSERT,UPDATE,DELETE ON lsb.Architecture TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Constant TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Header TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.HeaderGroup TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Interface TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.LibGroup TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.LGInt TO lsbadmin@localhost;
 # Only want to allow certain column to be edited
-#GRANT INSERT,UPDATE ON lsb.Library TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.Parameter TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.Standard TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.Type TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.TypeMember TO lsbadmin;
-GRANT INSERT,UPDATE,DELETE ON lsb.Version TO lsbadmin;
-GRANT INSERT,DELETE        ON lsb.VerInt TO lsbadmin;
+#GRANT INSERT,UPDATE ON lsb.Library TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Parameter TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Standard TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Type TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.TypeMember TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Version TO lsbadmin@localhost;
+GRANT INSERT,DELETE        ON lsb.VerInt TO lsbadmin@localhost;
 #
 # The rest should be "read Only" from the localhost
 #
-GRANT SELECT ON lsb.* TO lsbuser;
-GRANT SELECT ON lsb.* TO lsbadmin;
+GRANT SELECT ON lsb.* TO lsbuser@localhost;
+GRANT SELECT ON lsb.* TO lsbadmin@localhost;
 
 FLUSH PRIVILEGES;
