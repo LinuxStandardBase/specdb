13b7ce54600c65bb676e5c73ca868afc767837ec
anderson <>
2001-10-29 23:48:22 -0400

    Add perms for the rest of the tables

--- dbperms.sql
+++ dbperms.sql
@@ -12,20 +12,28 @@
 # Open up these tables for the LSB db admin
 #
 GRANT INSERT,UPDATE,DELETE ON lsb.Architecture TO lsbadmin@localhost;
+GRANT INSERT,DELETE        ON lsb.ArchInt TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.ArchType TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.CmdInt TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.CmdLib TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Command TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.Constant TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.DynamicEntries TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.ElfSections TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.Header TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.HeaderGroup TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.Interface TO lsbadmin@localhost;
-GRANT INSERT,UPDATE,DELETE ON lsb.LibGroup TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.LGInt TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.LibGroup TO lsbadmin@localhost;
 # Only want to allow certain column to be edited
 #GRANT INSERT,UPDATE ON lsb.Library TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.Parameter TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.SectionTypes TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.Standard TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.Type TO lsbadmin@localhost;
 GRANT INSERT,UPDATE,DELETE ON lsb.TypeMember TO lsbadmin@localhost;
-GRANT INSERT,UPDATE,DELETE ON lsb.Version TO lsbadmin@localhost;
 GRANT INSERT,DELETE        ON lsb.VerInt TO lsbadmin@localhost;
+GRANT INSERT,UPDATE,DELETE ON lsb.Version TO lsbadmin@localhost;
 #
 # The rest should be "read Only" from the localhost
 #
