a161d9515bc7eea886c3c2cc1a08497ce7964806
anderson <>
2002-03-11 13:58:13 -0400

    Add a new Type type, Array, and change a couple of things to use it.

--- Type.sql
+++ Type.sql
@@ -1,18 +1,18 @@
-# MySQL dump 8.16
-#
-# Host: localhost    Database: lsb
-#--------------------------------------------------------
-# Server version	3.23.47-log
+-- MySQL dump 8.21
+--
+-- Host: localhost    Database: lsb
+---------------------------------------------------------
+-- Server version	3.23.49-log
 
-#
-# Table structure for table 'Type'
-#
+--
+-- Table structure for table 'Type'
+--
 
 DROP TABLE IF EXISTS Type;
 CREATE TABLE Type (
   Tid int(10) NOT NULL auto_increment,
   Tname varchar(60) binary NOT NULL default '',
-  Ttype enum('Intrinsic','FuncPtr','Enum','Pointer','Typedef','Struct','Union','Unknown') NOT NULL default 'Unknown',
+  Ttype enum('Intrinsic','FuncPtr','Enum','Pointer','Typedef','Struct','Union','Array','Unknown') NOT NULL default 'Unknown',
   Tbasetype int(10) default NULL,
   Theadergroup int(10) NOT NULL default '0',
   Tcomment varchar(60) default NULL,
