2ea025d949be49f655b4c85bdfe13ddc260cd7cf
anderson <>
2002-04-25 13:38:43 -0400

    Snapshot containing IA64 specific symbol versions

--- ArchInt.sql
+++ ArchInt.sql
@@ -12,6 +12,7 @@ DROP TABLE IF EXISTS ArchInt;
 CREATE TABLE ArchInt (
   AIarch int(11) NOT NULL default '0',
   AIint int(11) NOT NULL default '0',
+  AIversion smallint(6) NOT NULL default '0',
   UNIQUE KEY k_AI (AIarch,AIint)
 ) TYPE=MyISAM;
 
