25dd5d891efadd7ae19e31a69007491ec879a740
cyeoh <>
2002-07-29 07:50:27 -0400

    Commit IA64 db updates (header files)

--- VerInt.sql
+++ VerInt.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: lsb
 ---------------------------------------------------------
--- Server version	3.23.51-log
+-- Server version	3.23.50-log
 
 --
 -- Table structure for table 'VerInt'
