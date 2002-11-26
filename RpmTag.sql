129b207a4e7fc2115e2c8eccc266d59773a43b02
anderson <>
2002-11-26 04:14:11 -0400

    Data for LSB v1.3pr7

--- RpmTag.sql
+++ RpmTag.sql
@@ -14,12 +14,12 @@ CREATE TABLE RpmTag (
   Rtag int(11) NOT NULL default '0',
   Rtype enum('NULL_TYPE','CHAR_TYPE','INT8','INT16','INT32','INT64','STRING','BIN','STRING_ARRAY','I18NSTRING') NOT NULL default 'BIN',
   Rcount int(11) NOT NULL default '0',
-  Rgroup enum('Private','Signature','Header','Other','Ignore','PackageInfo','InstallInfo','FileDetails','Dependencies') NOT NULL default 'Private',
+  Rgroup enum('Private','Signature','SigDigest','SigSigning','Header','Other','Ignore','PackageInfo','InstallInfo','FileDetails','Dependencies') NOT NULL default 'Private',
   Rstatus enum('Required','Optional','Deprecated','Obsolete','Reserved') NOT NULL default 'Required',
   Rdescription text NOT NULL,
   PRIMARY KEY  (Rid),
-  UNIQUE KEY Rname (Rname),
   UNIQUE KEY Rid (Rid),
+  UNIQUE KEY Rname (Rname),
   KEY Rtag (Rtag)
 ) TYPE=MyISAM;
 
