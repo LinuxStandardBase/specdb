5538dc6c431fb987310b58ad840f2fd740999885
cyeoh <>
2002-12-03 08:14:23 -0400

    S390x and S390 updates. Stuart has the lock

--- Command.sql
+++ Command.sql
@@ -17,7 +17,7 @@ CREATE TABLE Command (
   Cdocumented enum('Yes','No') NOT NULL default 'No',
   Ctested enum('Yes','No') NOT NULL default 'No',
   PRIMARY KEY  (Cid),
-  UNIQUE KEY Cname (Cname),
-  UNIQUE KEY Cpath (Cpath)
+  UNIQUE KEY Cpath (Cpath),
+  UNIQUE KEY Cname (Cname)
 ) TYPE=MyISAM;
 
