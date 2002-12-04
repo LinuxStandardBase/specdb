142d6827a7d25ff6cd34dc778005ed15f3dc0e2e
cyeoh <>
2002-12-04 07:56:22 -0400

    Header file updates for S390X

--- RpmTag.sql
+++ RpmTag.sql
@@ -18,8 +18,8 @@ CREATE TABLE RpmTag (
   Rstatus enum('Required','Optional','Deprecated','Obsolete','Reserved') NOT NULL default 'Required',
   Rdescription text NOT NULL,
   PRIMARY KEY  (Rid),
-  UNIQUE KEY Rname (Rname),
   UNIQUE KEY Rid (Rid),
+  UNIQUE KEY Rname (Rname),
   KEY Rtag (Rtag)
 ) TYPE=MyISAM;
 
