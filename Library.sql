74c45c2b533bd80a218d95fcdefe148e73e61119
anderson <>
2003-03-26 17:07:16 -0400

    Sync in to move master to base1 for a little while

--- Library.sql
+++ Library.sql
@@ -12,7 +12,7 @@ CREATE TABLE Library (
   Lid int(10) NOT NULL auto_increment,
   Lname varchar(60) binary NOT NULL default '',
   Lrunname varchar(60) binary NOT NULL default '',
-  Lstd enum('Yes','No','Future') NOT NULL default 'No',
+  Lstd enum('Yes','Future','No') NOT NULL default 'No',
   Larch int(11) NOT NULL default '1',
   PRIMARY KEY  (Lid),
   UNIQUE KEY k_lib (Lname,Lstd)
