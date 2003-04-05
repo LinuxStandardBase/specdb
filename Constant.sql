e6fd845b9c22aa53a053fa001780a6c0d7382974
anderson <>
2003-04-04 21:57:35 -0400

    Add a macro type Constant, where the value is anotehr macro

--- Constant.sql
+++ Constant.sql
@@ -11,7 +11,7 @@ DROP TABLE IF EXISTS Constant;
 CREATE TABLE Constant (
   Cid int(10) NOT NULL auto_increment,
   Cname varchar(80) binary NOT NULL default '',
-  Ctype enum('int','long','float','string','Unknown') NOT NULL default 'Unknown',
+  Ctype enum('int','long','float','string','macro','Unknown') NOT NULL default 'Unknown',
   Cheadgroup int(10) NOT NULL default '0',
   Ccomment varchar(60) default NULL,
   Cstd enum('Yes','No','SrcOnly') NOT NULL default 'No',
