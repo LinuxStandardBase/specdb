dab6e90f3e61bf1655d6860d3f762a9990345d8c
anderson <>
2004-05-11 02:24:21 -0400

    Sync in lot's of recent changes

--- Header.sql
+++ Header.sql
@@ -11,7 +11,7 @@ DROP TABLE IF EXISTS Header;
 CREATE TABLE Header (
   Hid int(10) NOT NULL auto_increment,
   Hname varchar(60) binary NOT NULL default '',
-  Hstd enum('Yes','No','SrcOnly') NOT NULL default 'No',
+  Hstd enum('Yes','No','SrcOnly','SrcError') NOT NULL default 'No',
   Hlib int(11) NOT NULL default '0',
   PRIMARY KEY  (Hid),
   UNIQUE KEY Hname (Hname)
