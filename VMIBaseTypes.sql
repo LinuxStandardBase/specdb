93e4233317e026b3d8a6c4cec962aadff120059d
anderson <>
2004-08-06 17:48:05 -0400

    DB Snapshot for LSB 2.0rc2

--- VMIBaseTypes.sql
+++ VMIBaseTypes.sql
@@ -12,7 +12,7 @@ CREATE TABLE VMIBaseTypes (
   VBTcid int(11) NOT NULL default '0',
   VBTpos int(11) NOT NULL default '0',
   VBTbasetype int(11) NOT NULL default '0',
-  VBTflags bigint(14) unsigned NOT NULL default '0',
+  VBTflags bigint(20) unsigned NOT NULL default '0',
   VBTaid int(11) NOT NULL default '1',
   PRIMARY KEY  (VBTcid,VBTpos,VBTaid),
   KEY VBTciid (VBTcid)
