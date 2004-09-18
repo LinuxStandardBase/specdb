0cab8f604c8c246f54e6ac3c5be0d7af79f8cbfa
nick <>
2004-09-17 23:54:08 -0400

    First pass at adding a short name -- see bug 277

--- Standard.sql
+++ Standard.sql
@@ -11,13 +11,15 @@ DROP TABLE IF EXISTS Standard;
 CREATE TABLE Standard (
   Sid tinyint(3) NOT NULL auto_increment,
   Sname varchar(60) NOT NULL default '',
-  Sfull varchar(255) default NULL,
+  Sfull blob default NULL,
   Surl varchar(255) default NULL,
   Scomment varchar(60) default NULL,
   Stype enum('Standard','Reference','Unknown') NOT NULL default 'Unknown',
   Sarch int(11) NOT NULL default '1',
+  Sshort varchar(60) NOT NULL default '',
   PRIMARY KEY  (Sid),
   UNIQUE KEY Sname (Sname),
-  KEY Stype (Stype)
+  KEY Stype (Stype),
+  KEY Sshort (Sshort)
 ) TYPE=MyISAM;
 
