3308aac76da8475f2e2eab585780b6ee675ab47f
nick <>
2005-09-02 20:13:29 -0400

    Update of ArchConst to handle long definitions.

--- ArchConst.sql
+++ ArchConst.sql
@@ -11,7 +11,7 @@ DROP TABLE IF EXISTS ArchConst;
 CREATE TABLE ArchConst (
   ACaid int(11) NOT NULL default '0',
   ACcid int(11) NOT NULL default '0',
-  ACvalue varchar(255) NOT NULL default '',
+  ACvalue text NOT NULL,
   UNIQUE KEY k_AC (ACaid,ACcid)
 ) TYPE=MyISAM;
 
