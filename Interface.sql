f22970bd93773ce6171cc52509faf3dd298126cd
Mats Wichmann <mats@linuxfoundation.org>
2006-01-18 18:41:18 -0400

    Resync: after the last two sets of changes not made through the live DB, this is the reload-and-dump of that data through the live DB.

--- Interface.sql
+++ Interface.sql
@@ -1,16 +1,7 @@
--- MySQL dump 10.9
+-- MySQL dump 9.11
 --
 -- Host: localhost    Database: lsb
 -- ------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `Interface`
@@ -19,7 +10,7 @@
 DROP TABLE IF EXISTS `Interface`;
 CREATE TABLE `Interface` (
   `Iid` int(10) NOT NULL auto_increment,
-  `Iname` varchar(255) character set latin1 collate latin1_bin NOT NULL default '',
+  `Iname` varchar(255) binary NOT NULL default '',
   `Istatus` enum('Included','Excluded','Defered','Unknown','SrcOnly','Deprecated','Withdrawn') NOT NULL default 'Unknown',
   `Itype` enum('Function','Data','Alias','Common','Unknown') NOT NULL default 'Unknown',
   `Istandard` int(10) NOT NULL default '0',
@@ -38,13 +29,5 @@ CREATE TABLE `Interface` (
   KEY `Istatus` (`Istatus`),
   KEY `Itype` (`Itype`),
   KEY `Iarch` (`Iarch`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+) TYPE=MyISAM;
 
--- Library.sql
+++ Library.sql
@@ -1,16 +1,7 @@
--- MySQL dump 10.9
+-- MySQL dump 9.11
 --
 -- Host: localhost    Database: lsb
 -- ------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `Library`
@@ -19,19 +10,11 @@
 DROP TABLE IF EXISTS `Library`;
 CREATE TABLE `Library` (
   `Lid` int(10) NOT NULL auto_increment,
-  `Lname` varchar(60) character set latin1 collate latin1_bin NOT NULL default '',
-  `Lrunname` varchar(60) character set latin1 collate latin1_bin NOT NULL default '',
+  `Lname` varchar(60) binary NOT NULL default '',
+  `Lrunname` varchar(60) binary NOT NULL default '',
   `Lstd` enum('Yes','Future','No') NOT NULL default 'No',
   `Larch` int(11) NOT NULL default '1',
   PRIMARY KEY  (`Lid`),
   UNIQUE KEY `k_lib` (`Lname`,`Lstd`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+) TYPE=MyISAM;
 
