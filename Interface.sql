fd9126ee4df2e37e791e6667dd77f0246947aeea
Denis Silakov <silakov@ispras.ru>
2007-06-05 05:32:10 -0700

    ISP RAS 0.7 Changeset

--- Interface.sql
+++ Interface.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.9
+-- MySQL dump 10.10
 --
 -- Host: localhost    Database: lsb
 -- ------------------------------------------------------
@@ -7,6 +7,8 @@
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
@@ -22,10 +24,10 @@ CREATE TABLE `Interface` (
   `Iname` varchar(255) character set latin1 collate latin1_bin NOT NULL default '',
   `Itype` enum('Function','Data','Alias','Common','Unknown') NOT NULL default 'Unknown',
   `Istandard` int(10) unsigned NOT NULL default '0',
+  `Irefspec` int(10) unsigned NOT NULL default '0',
   `Iheader` int(10) unsigned NOT NULL default '0',
   `Ireturn` int(10) unsigned NOT NULL default '0',
   `Idocumented` enum('Yes','No','Unknown') NOT NULL default 'Unknown',
-  `Itested` enum('Yes','No','Unknown') NOT NULL default 'Unknown',
   `Icomment` varchar(255) default NULL,
   `Icandidatefor` varchar(255) default NULL,
   `Iisdeprecated` enum('Yes','No','Unknown') default 'Unknown',
@@ -36,10 +38,10 @@ CREATE TABLE `Interface` (
   KEY `k_Iname` (`Iname`,`Iid`),
   KEY `Istandard` (`Istandard`),
   KEY `Itype` (`Itype`),
-  KEY `k_Itested` (`Itested`),
   KEY `k_Idocumented` (`Idocumented`),
   KEY `k_Iheader` (`Iheader`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=63091 DEFAULT CHARSET=latin1;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -49,3 +51,4 @@ CREATE TABLE `Interface` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2007-05-30 14:29:23
--- TestSuite.sql
+++ TestSuite.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.9
+-- MySQL dump 10.10
 --
 -- Host: localhost    Database: lsb
 -- ------------------------------------------------------
@@ -7,6 +7,8 @@
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
@@ -25,11 +27,15 @@ CREATE TABLE `TestSuite` (
   `TSstatus` enum('Yes','No','Unknown') NOT NULL default 'Unknown',
   `TSpartof` varchar(255) NOT NULL default '',
   `TSversion` varchar(255) NOT NULL default '',
-  `TSlsbversion` varchar(255) NOT NULL default '',
+  `TSappearedin` varchar(255) NOT NULL default '',
+  `TSwithdrawnin` varchar(255) default NULL,
   PRIMARY KEY  (`TSid`),
   KEY `TSid` (`TSid`,`TSname`,`TSversion`),
-  KEY `TSlsbversion` (`TSlsbversion`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  KEY `TSappearedin` (`TSappearedin`,`TSwithdrawnin`),
+  KEY `TSwithdrawnin` (`TSwithdrawnin`),
+  KEY `TSname` (`TSname`)
+) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -39,3 +45,4 @@ CREATE TABLE `TestSuite` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2007-05-30 14:29:27
